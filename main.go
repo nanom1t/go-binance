41c9592f4c0155437c17171ed3c49dd33a289f87
nanom1t <bitfire777@gmail.com>
2023-03-07 09:44:57 +0200

    Get exchange symbols

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"github.com/aiviaio/go-binance/v2"
+	"fmt"
+	"context"
+	"log"
+)
+
+const (
+	apiKey = "api key"
+	secretKey = "secret key"
+	symbolsNumber = 5
+)
 
 func main() {
-	fmt.Println("+++")
+	// get exchange symbols
+	symbols, err := GetExchangeSymbols(symbolsNumber)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println(symbols)
+}
+
+// Get exchange symbols
+func GetExchangeSymbols(count int) ([]string, error) {
+	client := binance.NewClient(apiKey, secretKey)
+	ctx := context.Background()
+
+	response, err := client.NewExchangeInfoService().Do(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	symbols := make([]string, 0, count)
+	if response.Symbols != nil {
+		for z := 0; z < count; z++ {
+			if z < len(response.Symbols) {
+				symbol := response.Symbols[z].Symbol
+				symbols = append(symbols, symbol)
+			}
+		}
+	}
+
+	return symbols, nil
 }
